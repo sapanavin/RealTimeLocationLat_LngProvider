3da440ff26afa5477c913c5bb32abdba59c35d25
sapanavin <116043155+sapanavin@users.noreply.github.com>
2024-03-29 13:12:07 -0500

    Trying to generate real time lat lng data by using browser's Geolocation API

---
+++ Backend_node/app.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const fs = require('fs');
+const bodyParser = require('body-parser');
+// Requiring module 
+const xlsx = require('xlsx') 
+  
+const app = express();
+const port = 3000;
+
+// Define the CORS options
+const corsOptions = {
+  credentials: true,
+  origin: ['*','http://localhost:3000', 'http://127.0.0.1:5500','http://localhost:5500'] // Whitelist the domains you want to allow
+};
+
+app.use(cors(corsOptions)); // Use the cors middleware with your options
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Initialize workbook and worksheet
+let workbook;
+let worksheet;
+const filePath = 'data.xlsx';
+
+// Load existing workbook or create new if doesn't exist
+if (fs.existsSync(filePath)) {
+    workbook = xlsx.readFile(filePath);
+    worksheet = workbook.Sheets[workbook.SheetNames[0]];
+} else {
+    workbook = xlsx.utils.book_new();
+    worksheet = xlsx.utils.aoa_to_sheet([['Count', 'Timestamp','Latitude', 'Longitude']]);
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+}
+
+var count= 0;
+
+// API endpoint to receive latitude and longitude data
+app.post('/save-data', (req, res) => {
+  // Initialize workbook and worksheet
+
+  var { timestamp,lat, lng } = req.body;
+  console.log("count, timestamp, lat, lng : ==> ",count++, timestamp, lat, lng );
+    lat=String(lat);
+    lng=String(lng);
+   xlsx.utils.sheet_add_aoa(worksheet, [[count, timestamp.toString(), lat.toString(), lng.toString()]], { origin: -1 });
+
+  // Write workbook to file
+  xlsx.writeFile(workbook, 'data.xlsx');
+
+  // Send response back to the HTML page
+  res.status(200).send('Data saved successfully');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
